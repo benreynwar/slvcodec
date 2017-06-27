a477d464b19aed8649fc677b378b5b49e2eb2185
Ben Reynwar <ben@reynwar.net>
2017-06-27 14:59:12 -0700

    Fix bug in array template.

--- slvcodec/templates/slvcodec_array_template.vhd
+++ slvcodec/templates/slvcodec_array_template.vhd
@@ -12,10 +12,12 @@
   function from_slvcodec (constant slv: std_logic_vector) return {{type}} is
     constant W: natural := {{subtype_width}};
     constant N: natural := slv'length/W;
+    variable mapped: std_logic_vector(slv'length-1 downto 0);
     variable output: {{type}}{% if unconstrained %}(N-1 downto 0){% endif %};
   begin
+    mapped := slv;
     for ii in 0 to N-1 loop
-      output(ii) := from_slvcodec(slv((ii+1)*W-1 downto ii*W));
+      output(ii) := from_slvcodec(mapped((ii+1)*W-1 downto ii*W));
     end loop;
     return output; 
   end function;
