e617e0eb4a27e3b87118d5ad114171bc957771b7
Ben Reynwar <ben@reynwar.net>
2017-07-17 19:34:37 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = "slvcodec",
+    packages=['slvcodec',
+              ],
+    use_scm_version = {
+        "relative_to": __file__,
+        "write_to": "slvcodec/version.py",
+    },
+    author = "Ben Reynwar",
+    author_email = "ben@reynwar.net",
+    description = ("slvcodec is a package of utilities for generating VHDL to convert to and from std_logic_vector, as well as utilties to create testbenches described by python."),
+    license = "MIT",
+    keywords = ["VHDL", "hdl", "rtl", "FPGA", "ASIC", "Xilinx", "Altera"],
+    url = "https://github.com/benreynwar/slvcodec",
+    install_requires=[
+        'jinja2>=2.8',
+        'vunit==2.0.0',
+    ],
+)
