b020bc5ca3d58b48dad685ba4698374d9c85c32f
Ben Reynwar <ben@reynwar.net>
2017-08-07 21:18:00 -0700

    Adding additional utility conversion functions.

--- slvcodec/conversions.py
+++ slvcodec/conversions.py
@@ -33,3 +33,38 @@ def uint_to_list_of_uints(uint, size, width):
         residual = residual >> width
     assert(residual == 0)
     return output
+
+
+def sint_to_uint(sint, width):
+    uint = sint + pow(2, width-1)
+    return uint
+
+
+def uint_to_sint(uint, width):
+    sint = uint - pow(2, width-1)
+    return sint
+
+
+def list_of_sints_to_uint(list_of_sints, width):
+    list_of_uints = [sint_to_uint(sint, width) for sint in list_of_sints]
+    uint = list_of_uints_to_uint(list_of_uints, width)
+    return uint
+
+
+def uint_to_list_of_sints(uint, size, width):
+    list_of_uints = uint_to_list_of_uints(uint, size, width)
+    list_of_sints = [uint_to_sint(uint, width) for uint in list_of_uints]
+    return list_of_sints
+
+
+def slv_to_uint(slv):
+    total = 0
+    f = 1
+    for ch in reversed(slv):
+        if ch not in ('0', '1'):
+            total = None
+        if total is not None:
+            if ch == '1':
+                total += f
+            f *= 2
+    return total
