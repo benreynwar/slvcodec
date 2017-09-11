dc3143c5f13caff34c4ddde4d022e4f08a8c5cd2
Ben Reynwar <ben@reynwar.net>
2017-09-10 18:51:11 -0700

    Add a demo script for generating slvcodec package or testbench without running python test.

---
+++ examples/complex_demo.py
@@ -0,0 +1,32 @@
+import os
+
+from slvcodec import filetestbench_generator
+
+
+thisdir = os.path.dirname(__file__)
+
+
+def make_slvcodec_package():
+    complex_pkg_fn = os.path.join(thisdir, 'complex_pkg.vhd')
+    directory = os.path.join(thisdir, 'generated')
+    os.mkdir(directory)
+    slvcodec_files = filetestbench_generator.add_slvcodec_files(directory, [complex_pkg_fn])
+    return slvcodec_files
+
+
+def make_complex_mag2_testbench():
+    base_filenames = [
+        os.path.join(thisdir, 'complex_pkg.vhd'),
+        os.path.join(thisdir, 'complex_mag2.vhd'),
+        ]
+    slvcodec_fns = make_slvcodec_package()
+    with_slvcodec_fns = base_filenames + slvcodec_fns
+    directory = os.path.join(thisdir, 'generated')
+    generated_fns, resolved = filetestbench_generator.prepare_files(
+        directory=directory, filenames=with_slvcodec_fns,
+        top_entity='complex_mag2')
+    return generated_fns
+
+if __name__ == '__main__':
+    # make_slvcodec_package()
+    make_complex_mag2_testbench()
