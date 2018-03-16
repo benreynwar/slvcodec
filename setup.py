f6d1bb2c920251de6b41225b513cf3aba5df2530
Ben Reynwar <ben@reynwar.net>
2018-03-15 21:21:47 -0700

    Update slvcodec dependencies.

--- setup.py
+++ setup.py
@@ -19,9 +19,13 @@ setup(
         'jinja2>=2.8',
         'pytest',
         'fusesoc_generators',
+        'pyvivado',
         'vunit-hdl',
+        'axilent==0.0.0',
     ],
     dependency_links=[
         'git+https://github.com/benreynwar/fusesoc_generators@9a374ae8fdfdbe0f1448d7973783464614990d9c#egg=fusesoc_generators-0.0.0',
+        'git+https://github.com/benreynwar/pyvivado@680364d2fbd04679ef06ea229702e0e6e5394af3#egg=pyvivado-0.0.0',
+        'git+https://github.com/benreynwar/axilent@963c6824f2fdbd2a3beaef2682fb7461ddcb5a86#egg=axilent-0.0.0',
     ],
 )
