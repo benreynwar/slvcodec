9ce9ae868a342601428ee0959e95363166d407f7
Ben Reynwar <ben@reynwar.net>
2018-11-13 10:20:19 -0700

    Add function logceil_1to0

--- slvcodec/__init__.py
+++ slvcodec/__init__.py
@@ -2,5 +2,6 @@ from slvcodec import math_parser, filetestbench_generator, vhdl_parser
 
 
 logceil = math_parser.logceil
+logceil_1to0 = math_parser.logceil_1to0
 add_slvcodec_files = filetestbench_generator.add_slvcodec_files
 parse_and_resolve_files = vhdl_parser.parse_and_resolve_files
